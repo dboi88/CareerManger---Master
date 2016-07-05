428ce32db25f1c11455eea8d2aa50441835a6fb1
dboi88 <danielfay88@gmail.com>
2016-07-05 18:37:30 +0100

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,44 +1,72 @@
-namespace MissionList
-{
-    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
     public class MissionList : MonoBehaviourExtended
     {
-      public bool Drawaddmission
-      {
-      get
-      {
-          return drawaddmission;
-      }
-      set
-      {
-          drawaddmission = value;
-      }
-      public bool drawaddmission = false;
-      
+        public bool drawaddmission = false;
+        void OnGUI()
+        {
+            OnDraw();
+        }
+        private void OnDraw()
+        {
+            /// some code
+        }
+        private void OnWindow(int windowId)
+        {
+            if (GUILayout.Button("Add Mission"))
+            {
+                drawaddmission = true;
+                LogFormatted("on button press " + drawaddmission);
+            }
+            if (GUILayout.Button("make addmissionwindow false"))
+            {
+                drawaddmission = false;
+            }
+
+            GUI.DragWindow();
+        }
     }
-    
-    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
     public class AddMissionWindow : MissionList
     {
-      if (Drawaddmission)
+        private string addmissionstring = "Mission Name";   
+
+        internal override void Update()
+        {
+            LogFormatted("" + drawaddmission);
+        }
+
+        void OnGUI()
+        {
             {
-            ///some code
-            Drawadmission = false;
+                OnDrawAddMission();
             }
-  }
-  /// provided by jimroberts
-  change your MissionList class to this
-public class MissionList : MonoBehaviourExtended
-{
-    public bool DrawAddMission = false
+        }
+
+        public void OnDrawAddMission()
+        {
+            ///LogFormatted("OnDrawAddMission = fired" + drawaddmission);
+            if (drawaddmission)
+            {
+                LogFormatted("OnDrawAddMission = true & fired");
+                _addmissionwindowPosition = GUILayout.Window(03, _addmissionwindowPosition, OnWindow, "Title");
+
+                if (_addmissionwindowPosition.x == 0f && -_addmissionwindowPosition.y == 0f)
+                    _addmissionwindowPosition = _addmissionwindowPosition.CenterScreen();
+            }
+        }
+
+        public void OnWindow(int windowId)
+        {
+            addmissionstring = GUILayout.TextField(addmissionstring, 25);
+            if (GUILayout.Button("Add Mission"))
+            {
+                drawaddmission = false;
+                Debug.Log(addmissionstring);
+                missionlist.Add(addmissionstring);
+                addmissionstring = "Mission Name";
+            }
+
+
+            GUI.DragWindow();
+        }
+    }
+
 }
-and your AddMissionWindow should allow you to use this
-public class AddMissionWindow : MissionList
-{
-      if (DrawAddMission)
-      {
-            ///some code
-            DrawAddMission = false;
-      }
-}
-      
